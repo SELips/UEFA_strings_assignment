c4e527ff6f13c085ebe75a229f62e0fca7f12b6d
SELips <108814671+SELips@users.noreply.github.com>
2022-07-14 11:04:06 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+# Do not modify these lines
+__winc_id__ = '71dd124b4a6e4d268f5973db521394ee'
+__human_name__ = 'strings'
+
+# Add your code after this line
+
+# Part 1
+first_player_scored = 'Ruud Gullit'
+second_player_scored = 'Marco van Basten' 
+
+goal_0 = 32
+goal_1 = 54
+
+scorers = first_player_scored + " " + str(goal_0) +", " + second_player_scored +" " + str(goal_1)
+
+report = f'{first_player_scored} scored in the {goal_0}nd minute\n{second_player_scored} scored in the {goal_1}th minute'
+
+
+# Part 2
+player = 'Erwin Koeman'
+first_name = player[player.find('Erwin'):player.find(' ')]
+last_name_len = len(player[player.find('Koeman'):])
+name_short = f'{first_name[0]}.{player[-1-last_name_len:]}'
+chant = f'{first_name}! ' * (len(first_name)-1) + f'{first_name}!'
+good_chant = chant[-1]!=" "
